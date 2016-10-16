6444466e7c5eea0d62680c48d4512093c1de4ca6
Martin Homuth <martin@martinhomuth.de>
2016-10-16 19:43:19 +0200

    add common header
    Signed-off-by: Martin Homuth <martin@martinhomuth.de>

---
+++ include/allhead.h
@@ -0,0 +1,16 @@
+#ifndef __ALLHEAD_H
+#define __ALLHEAD_H
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#define enum { FALSE, TRUE } Boolean;
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+#endif
