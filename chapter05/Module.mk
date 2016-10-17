c5ca4ef0bbfc1b8c50daf97e23a7b9b8d875b580
Martin Homuth <martin.homuth@emlix.com>
2016-10-17 18:19:12 +0200

    update Makefile (not working yet)
    Signed-off-by: Martin Homuth <martin@martinhomuth.de>

---
+++ chapter05/Module.mk
@@ -0,0 +1,8 @@
+
+SRC += chapter05/exercise01.c
+SRC += chapter05/exercise02.c
+SRC += chapter05/exercise03.c
+SRC += chapter05/exercise04.c
+SRC += chapter05/exercise05.c
+SRC += chapter05/exercise06.c
+SRC += chapter05/exercise07.c
---
+++ chapter06/Module.mk
@@ -0,0 +1,5 @@
+
+SRC += chapter06/pathname.c
+SRC += chapter06/exercise01.c
+SRC += chapter06/exercise02.c
+SRC += chapter06/exercise03.c
